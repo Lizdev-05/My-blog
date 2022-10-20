1b7e001473c9d58a51e189c750258055c9153cb8
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-10-20 12:31:31 +0100

    add method for comment and likes counter in comment rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
     belongs_to :post
 belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+def update_comments_counter
+    post.increment!(:comments_counter)
+  end
+
+
+private
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
