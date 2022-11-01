7f0851bb70714d980ab5b6ef35b81fafe5c69fc0
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-01 18:30:44 +0100

    user#show test to render recent posts

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -36,5 +36,14 @@ RSpec.describe 'renders users Show Page', type: :system, js:true do
 
   
 
+  it 'it should render the view all user posts link' do
+    expect(page).to have_link('See all posts')
+  end
 
+  it 'should render three recent posts' do
+    expect(page).to have_content(@user.return_recent_posts[0])
+    expect(page).to have_content(@user.return_recent_posts[1])
+    expect(page).to have_content(@user.return_recent_posts[2])
+    expect(page).to have_no_content(Post.where(author: @user))
+  end
 end
