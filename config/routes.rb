6c3ebb0124b078fa25d9d131b5c0676eecbd73f4
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-04 20:35:05 +0100

    add route for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
-# Rails.application.routes.draw do
-#   devise_for :users
+Rails.application.routes.draw do
+  devise_for :users
   
-#     namespace :api do
-#       namespace :v1 do
-#         post :auth, to: 'authentication#create'
-#         resources :users, only: [] do
-#           resources :posts, only: [:index] do
-#             resources :comments, only: [:index, :create]
-#           end
-#         end
-#       end
-#     end
+    namespace :api do
+      namespace :v1 do
+        post :auth, to: 'authentication#create'
+        resources :users, only: [] do
+          resources :posts, only: [:index] do
+            resources :comments, only: [:index, :create]
+          end
+        end
+      end
+    end
 
-#   resources :users, only: [:index, :show] do
-#     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-#       resources :comments, only: [:new, :create, :destroy]
-#       resources :likes, only: [:create]
-#     end
-#   end
-#     # devise_for :users, controllers: { confirmations: 'users/confirmations' }
-#     root to: 'users#index'
-# end
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
+  end
+    # devise_for :users, controllers: { confirmations: 'users/confirmations' }
+    root to: 'users#index'
+end
